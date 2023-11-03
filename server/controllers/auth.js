4c41895a012406bea12a4cf269b9c755b01a4490
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-11-02 21:15:43 -0600

    Part 3 complete - Added JWT token and authentication middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,25 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.SECRET
+
+const createToken = (username, id) => {
+    return jwt.sign(
+        {
+            username,
+            id
+        },
+        SECRET,
+        {
+            expiresIn: '2 days'
+        }
+    )
+}
+
 module.exports = {
     login: async (req, res) => {
-        console.log('login')
-        res.sendStatus(200)
+        let { username, password } = req.body
+        const token = createToken(username, password)
+        res.sendStatus(200).send(token)
     },
     register: async (req, res) => {
         console.log('register')
--- server/index.js
+++ server/index.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const { getAllPosts, getCurrentUserPosts, deletePost } = require('./controllers/posts')
-const app = express()
-const PORT = process.env.Port || 4004
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.Port || 4004;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
 
+const {
+  getAllPosts,
+  getCurrentUserPosts,
+  addPost,
+  editPost,
+  deletePost,
+} = require("./controllers/posts");
+const { register, login } = require("./controllers/auth");
+const { isAuthenticated } = require("./middleware/isAuthenticated");
 
+app.post("/register", register);
+app.post("/login", login);
+app.post("/posts", isAuthenticated, addPost);
 
-const {getAllPosts, getCurrentUserPosts, addPost, editPost, deletePost} = require('./controllers/posts')
-const {register, login} = require('./controllers/auth')
+app.get("/posts", getAllPosts);
+app.get("/userposts/:userId", getCurrentUserPosts);
 
-app.post('/register', register)
-app.post('/login', login)
-app.post('/posts', addPost)
+app.put("posts/:id", isAuthenticated, editPost);
 
-app.get('/posts', getAllPosts)
-app.get('/userposts/:userId', getCurrentUserPosts)
-
-app.put('posts/:id', editPost)
-
-app.delete('/posts/:id', deletePost)
-
-
+app.delete("/posts/:id", isAuthenticated, deletePost);
